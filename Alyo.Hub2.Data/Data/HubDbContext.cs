640cea2005ace600d179bb674d7308d5eb457a85
yilmazalizadeh <y.alizadeh@hotmail.com>
2024-02-11 12:06:24 +0000

    init

---
+++ Alyo.Hub2.Data/Data/HubDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Alyo.Hub2.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Alyo.Hub2.Data.Data;
+
+public class HubDbContext : DbContext
+{
+    public DbSet<Asset> Assets => Set<Asset>();
+}
---
+++ Alyo.Hub2.Data/Models/Asset.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Alyo.Hub2.Data.Models;
+
+public class Asset
+{
+    [Key] public Guid Id { get; init; }
+
+    [StringLength(500, MinimumLength = 5)]
+    [Required]
+    public string Title { get; init; } = string.Empty;
+
+    [StringLength(1000)] public string? FarsiDescription { get; init; }
+
+    [StringLength(1000)] public string? Description { get; init; }
+
+    [StringLength(500, MinimumLength = 10)]
+    [Required]
+    public string Thumbnail { get; init; } = null!;
+
+    [StringLength(500, MinimumLength = 10)]
+    [Required]
+    public string VideoUrl { get; init; } = null!;
+
+    [StringLength(500, MinimumLength = 10)]
+    [Required]
+    public string DownloadUrl { get; init; } = null!;
+}
